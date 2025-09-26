a0a659bf7a87bc0346dd8e462216b0609bc24cb4
Enne Jim Pessoa <ennefofe10@gmail.com>
2025-09-26 16:37:03 -0300

    feat: add meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,38 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "Enne Jim Pessoa",
   description: "Portfólio web sobre mim e meus trabalhos enquanto UI/UX designer e desenvolvedore front-end",
+  authors: [{ name: "Enne Jim Pessoa", url: "https://enne-jim-pessoa.vercel.app" }],
+  openGraph: {
+    type: "website",
+    title: "Enne Jim Pessoa",
+    description: "Portfólio web sobre mim e meus trabalhos enquanto UI/UX designer e desenvolvedore front-end",
+    images: "https://github.com/Enne-Amore/enne-jim-pessoa/blob/main/public/favicon/logo.png?raw=true",
+    url: "https://enne-jim-pessoa.vercel.app",
+    siteName: "Enne Jim Pessoa"
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "https://enne-jim-pessoa.vercel.app",
+    title: "Enne Jim Pessoa",
+    description: "Portfólio web sobre mim e meus trabalhos enquanto UI/UX designer e desenvolvedore front-end",
+    images: "https://github.com/Enne-Amore/enne-jim-pessoa/blob/main/public/favicon/logo.png?raw=true",
+  },
+  icons: {
+    icon: "/favicon/favicon-32x32.png",
+    apple: "/favicon/apple-touch-icon.png",
+  },
+  manifest: "/favicon/site.webmanifest",
+  verification: {
+    google: ""
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
 };
 
 export default function RootLayout({

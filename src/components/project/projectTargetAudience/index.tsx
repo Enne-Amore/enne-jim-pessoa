d7a080e7705a84dbd28871620d8f5eae751a0022
Enne Jim Pessoa <ennefofe10@gmail.com>
2025-11-25 17:23:33 -0300

    style: update target audience section of cnbpb project

--- src/components/project/projectTargetAudience/index.tsx
+++ src/components/project/projectTargetAudience/index.tsx
@@ -7,27 +7,27 @@ export default function ProjectTargetAudience() {
       className={`flex flex-col justify-center items-center gap-12 xl:w-[1074px] xl:flex-row xl:justify-between`}
     >
       <div
-        className={`bg-[#FFFEF6]/30 flex flex-col items-center gap-1.5 border border-[#040008] rounded-4xl pl-10 pr-5 pt-7 pb-6 xl:pl-14 xl:pr-6 xl:pt-8 xl:pb-7`}
+        className={`space-y-1 xl:pl-6`}
       >
         <h2
-          className={`font-geometos-rounded text-28px text-shadow-2xs bg-gradient-to-r from-[#713CA6] to-[#6A4EA6] text-transparent bg-clip-text -ml-6 xl:text-32px xl:-ml-8`}
+          className={`font-geometos-rounded text-28px text-center text-shadow-2xs bg-gradient-to-r from-[#713CA6] to-[#6A4EA6] text-transparent bg-clip-text -ml-6 xl:text-32px xl:-ml-8`}
         >
           Público-Alvo
         </h2>
 
         <ul
-          className={`w-[255px] list-['⭐'] list-outside space-y-2 xl:w-[320px]`}
+          className={`w-[260px] list-['⭐'] list-outside space-y-3 xl:w-[320px]`}
         >
           <li
-            className={`font-open-dyslexic text-xl text-shadow-2xs pl-0.5 xl:pl-1 xl:text-2xl`}
+            className={`font-open-dyslexic text-xl text-shadow-2xs pl-2 xl:pl-1 xl:text-2xl`}
           >
             Comunidade não-binária
           </li>
 
           <li
-            className={`font-open-dyslexic text-xl text-shadow-2xs pl-0.5 xl:pl-1 xl:text-2xl`}
+            className={`font-open-dyslexic text-xl text-shadow-2xs pl-2 xl:pl-1 xl:text-2xl`}
           >
-            Comunidade residente do Estado da Paraíba
+            Residentes da Paraíba
           </li>
         </ul>
       </div>
